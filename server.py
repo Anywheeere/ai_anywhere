81a1ecfc16ad91ab82698eebcca0dc8203d0c63b
plain127 <plain_k1207@naver.com>
2024-08-30 17:57:40 +0900

    complete server test

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Body
 from fastapi.responses import JSONResponse, FileResponse
-import uvicorn
+import uvicorn, json
 
 app = FastAPI()
 
-@app.post('/docent')
-async def docent(text:str=Form(...)):
-    docent = text
+@app.get('/docent')
+async def docent(text:dict=Body(...)):
+    docent = text.get('text')
     audio = './result.wav'
     return JSONResponse(content={'docent':docent, 'audio':audio})
 
-@app.post('/audio')
-async def audio(path:str=Form(...)):
-    audio = './result.wav'
+@app.get('/audio')
+async def audio(path:dict=Body(...)):
+    audio = path.get('path')
     return FileResponse(audio, media_type='audio/wav')
   
  
