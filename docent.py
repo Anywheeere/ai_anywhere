6c0770c3ab476978fdef84c5123a40a912c8b0a7
plain127 <plain_k1207@naver.com>
2024-09-06 20:21:48 +0900

    making vector db

--- docent.py
+++ docent.py
@@ -21,6 +21,7 @@ class AiDocent:
             너는 유명한 관광가이드야. 
             {self.input}이 도시 이름이 아니라면 이 프롬프트 내용은 무시해.
             {self.input}이 도시 이름이면 그 도시의 주변 명소와 가까운 랜드마크들이 무엇이 있는지 말해.
+            그리고 출력에 '\n'같은 기호들은 넣지마.
         '''
         
         proto_landmark_prompt = f'''
@@ -32,6 +33,7 @@ class AiDocent:
             그리고 {self.input}이 랜드마크면 {self.input}의 현재에 대해서 설명해.
             그리고 {self.input}이 랜드마크면 {self.input}의 과거와 현재를 통해 미래에 어떤 모습일지 외형적, 내형적으로 추론해서 종합적으로 설명해.            
             그리고 {self.input}이 랜드마크면 위의 내용들을 모두 종합하고 추론해서 설명해.
+            그리고 출력에 '\n'같은 기호들은 넣지마.
         '''
         
         memory = ConversationBufferMemory()
